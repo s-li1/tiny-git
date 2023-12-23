7441d3996f4ce51c6ce47127618572eb68e369f7
s-li1 <stevenli8892@hotmail.com.au>
2023-12-24 09:26:45 +1100

    save blob object and print objects

--- tgit/cli.py
+++ tgit/cli.py
@@ -1,8 +1,8 @@
 import argparse
 import os
+import sys
 
 from . import data
-
 
 def main():
     args = parse_args()
@@ -17,8 +17,23 @@ def parse_args():
     init_parser = commands.add_parser('init')
     init_parser.set_defaults(func=init)
 
+    hash_object_parser = commands.add_parser('hash-object')
+    hash_object_parser.set_defaults(func=hash_object)
+    hash_object_parser.add_argument('file')
+
+    cat_file_parser = commands.add_parser('cat-file')
+    cat_file_parser.set_defaults(func=cat_file)
+    cat_file_parser.add_argument('object')
+    
     return parser.parse_args()
 
 def init(args):
     data.init()
     print(f'Initialized empty tgit repo in {os.path.join(os.getcwd(), data.TGIT_DIR)}')
+
+def hash_object(args):
+    with open(args.file, 'rb') as f:
+        print(data.hash_object(f.read()))
+
+def cat_file(args):
+    sys.stdout.buffer.write(data.get_object(args.object))
--- tgit/data.py
+++ tgit/data.py
@@ -1,6 +1,18 @@
+import hashlib
 import os
 
 TGIT_DIR = '.tgit'
 
 def init():
     os.mkdir(TGIT_DIR)
+    os.mkdir(os.path.join(TGIT_DIR, 'objects'))
+
+def hash_object(data):
+    oid = hashlib.sha1(data).hexdigest()
+    with open(os.path.join(TGIT_DIR, 'objects', oid), 'wb') as f_obj:
+        f_obj.write(data)
+    return oid
+
+def get_object(oid):
+    with open(os.path.join(TGIT_DIR, 'objects', oid), 'rb') as f_obj:
+        return f_obj.read()
