8a183b6ed016ce0157ab58c6dc979e48cd5dbb12
s-li1 <stevenli8892@hotmail.com.au>
2023-12-25 09:26:55 +1100

    add type to obj

--- tgit/data.py
+++ tgit/data.py
@@ -2,17 +2,27 @@ import hashlib
 import os
 
 TGIT_DIR = '.tgit'
+NULL = b'\x00'
 
 def init():
     os.mkdir(TGIT_DIR)
     os.mkdir(os.path.join(TGIT_DIR, 'objects'))
 
-def hash_object(data):
-    oid = hashlib.sha1(data).hexdigest()
+def hash_object(data, type='blob'):
+    obj = type.encode() + NULL + data
+    oid = hashlib.sha1(obj).hexdigest()
     with open(os.path.join(TGIT_DIR, 'objects', oid), 'wb') as f_obj:
-        f_obj.write(data)
+        f_obj.write(obj)
     return oid
 
-def get_object(oid):
+def get_object(oid, expected=None):
     with open(os.path.join(TGIT_DIR, 'objects', oid), 'rb') as f_obj:
-        return f_obj.read()
+            obj = f_obj.read()
+    
+    type, _, content = obj.partition(NULL)
+    type = type.decode()
+
+    if expected is not None and type != expected:
+        raise ValueError(f'Expected {expected}, got {type}')
+
+    return content
